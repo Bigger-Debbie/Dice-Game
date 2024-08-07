56047b92935c173e3f14c9aa3986e0ab84b1def2
Alex  Ross <alexross@Alexs-Mac-mini.local>
2024-08-06 21:26:34 -0400

    Finished roll button functionality. Started hold button functionality.

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -31,8 +35,25 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // switch to the next player
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
   }
+});
+
+btnHold.addEventListener("click", function () {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. check if player's score is >= 100, if so - finish the game
+
+  // 3. Switch to the next player
 });
